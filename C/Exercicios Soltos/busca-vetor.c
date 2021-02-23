537321d2f9f7daf6d2757dde9862098b14b692cd
Nicolas Marques <nicolasmarquesjf@hotmail.com>
2021-02-23 20:13:35 -0300

    Update busca-vetor.c

--- C/Exercicios Soltos/busca-vetor.c
+++ C/Exercicios Soltos/busca-vetor.c
@@ -1,31 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <locale.h>
 
 int main() {
-    setlocale(LC_ALL, "Portuguese");
+    
     int i = 0, finder;
     int vet[15];
     
-    for(i = 0; i < 15; i++){
+    for(i = 0; i < 15; i++)
+    {
         printf("Digite um numero a popular o vetor[%i]: \n", i);
         scanf("%i", &vet[i]);
     }
+    
     system ("cls || clear");
     printf ("Digite um numero a ser buscado no vetor: ");
     scanf ("%i", &finder);
 
-    for(i = 0; i < 15; i++){
-        
+    for(i = 0; i < 15; i++)
+    {        
         if (finder == vet[i])
         {            
-            printf ("A posiçao do vetor que contenha o numero buscado eh vet[%i]", i);
-        }
-        else
-        {
-        system ("cls || clear");
-        printf ("Numero nao encontrado no vetor!.");
-        }
+            printf ("A posicao do vetor que contenha o numero buscado eh vet[%i]", i);
+        }     
     }
 
     return 0;
