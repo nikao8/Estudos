04d519c873c4bda3e659b841fe3f568a388624a7
nikao8 <nicolasmarquesjf@hotmail.com>
2021-04-02 20:17:56 -0300

    att python

---
+++ Python/Exercicios Soltos/contabasica.py
@@ -0,0 +1,5 @@
+print ("Insira em que ano voce nasceu: \n\n")
+x = int(input())
+
+print ("Voce tem: ", 2021 - x)
+# Teste de comments
--- Python/contabasica.py
+++
@@ -1,5 +0,0 @@
-print ("Insira em que ano voce nasceu: \n\n")
-x = int(input())
-
-print ("Voce tem: ", 2021 - x)
-# Teste de comments
