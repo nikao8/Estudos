23ecbd4632e538cd5ed2e570273ed0ac75e0d15a
nikao8 <nicolasmarquesjf@hotmail.com>
2021-07-21 22:03:53 -0300

    primeiro codigo em c# curso alura pt 1

---
+++ CSharp Alura/CSharp parte 1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _1_PrimeiroProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá mundo!");
+            Console.WriteLine("A execução do programa foi finalizada. Tecle enter para sair");
+
+            Console.ReadLine();
+        }
+    }
+}
