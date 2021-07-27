48cd43e487a88dc48303d285b4741f28d92fd8cc
nikao8 <nicolasmarquesjf@hotmail.com>
2021-07-27 19:05:43 -0300

    update e add main.py e criando var em csharp

---
+++ CSharp Alura/CSharp parte 1/2-CriandoVariaveis.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _2_CriandoVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 2 - Criando as primeiras variáveis...");
+
+            // Declaração de variaveis inteiras:
+            int idade;
+            idade = 19;
+
+            Console.WriteLine(idade);
+
+            idade = 19 + 5;
+
+            Console.WriteLine("O valor da variável idade é: " + idade + " anos.");
+
+            // Declaração de variaveis decimais/pontos flutuantes:
+
+            double salario;
+            salario = 1200.0;
+
+            Console.WriteLine("O valor da variavel salario é: " + salario + " reais.");
+
+            salario = 15 / 2;
+
+            Console.WriteLine("Divisao de numeros inteiros em uma variavel tipo double: " + salario);
+
+            salario = 15.0 / 2;
+
+            Console.WriteLine("Divisao de numeros decimais em uma variavel tipo double: " + salario);
+            Console.WriteLine("Execução finalizada. Tecle enter para sair.");
+            Console.ReadLine();
+        }
+    }
+}
--- Python/SDC Discord BOT/main.py
+++ Python/SDC Discord BOT/main.py
@@ -33,38 +33,45 @@ async def on_ready():
   game = discord.Game("github.com/nikao8")
   await client.change_presence(status=None, activity=game)
   print('Logado como {0.user}'.format(client))
+  await send_video()
 
 @client.event
-async def on_message(message): # SO EXECUTA QUANDO É ENVIADO UMA MENSAGEM
+async def send_video():
   
-  if message.content.startswith('>>setchannel'):
-    channel_ids = message.channel.id
-    print(channel_ids)
-    await message.channel.send('Sexta dos crias setadas neste canal!')
+  #FUNCIONANDO !!!
+  text_channel_list = []
+  channel_id = []
+  for guild in client.guilds:
+    for channel in guild.text_channels:
+        text_channel_list.append(channel)
+        channel_id = channel.id
+        print(channel_id)
+  
+  infinity_loop = 1
 
-    infinity_loop = 1
+  while (infinity_loop == 1):
+    day = date.today()
+    d1 = day.strftime('%Y/%m/%d')
+
+    ano = d1[0:4]
+    mes = d1[5:7]
+    dia = d1[8:10]
+
+    if(mes.startswith('0')):
+      mes = d1[6:7]
+
+    if(dia.startswith('0')):
+      dia = d1[9:10]
     
-    while (infinity_loop == 1):
-      day = date.today()
-      d1 = day.strftime('%Y/%m/%d')
+    diaStr = datetime.date(int(ano),int(mes),int(dia)).strftime('%A')
+    horario = time.localtime()
+    horario_agora = time.strftime("%H:%M:%S", horario) 
 
-      ano = d1[0:4]
-      mes = d1[5:7]
-      dia = d1[8:10]
-
-      if(mes.startswith('0')):
-        mes = d1[6:7]
-
-      if(dia.startswith('0')):
-        dia = d1[9:10]
-      
-      diaStr = datetime.date(int(ano),int(mes),int(dia)).strftime('%A')
-      horario = time.localtime()
-      horario_agora = time.strftime("%H:%M:%S", horario) 
-  
-      if(horario_agora >= '23:29:00' and horario_agora <= '23:29:03' and diaStr == 'Friday'):
-        await client.get_channel(channel_ids).send('https://cdn.discordapp.com/attachments/767871106035875880/865399647682166864/sexta_dos_crias_nessa_porra_caralho_tavam_com_saudades_-_-.mp4')
-        time.sleep(9)
-
+    if(horario_agora >= '17:00:00' and horario_agora <= '17:00:05' and diaStr == 'Friday'):
+      for guild in client.guilds:
+        for channel in guild.text_channels:
+          await client.get_channel(channel_id).send('https://cdn.discordapp.com/attachments/767871106035875880/865399647682166864/sexta_dos_crias_nessa_porra_caralho_tavam_com_saudades_-_-.mp4')
+      time.sleep(15)
+          
 # INVITE LINK: https://discord.com/api/oauth2/authorize?client_id=865350229318828113&permissions=8&scope=bot
 client.run(os.getenv('TOKEN'))
