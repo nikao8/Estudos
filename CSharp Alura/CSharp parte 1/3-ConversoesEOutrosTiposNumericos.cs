345287f727a06ac8e494cac0671d58a429cece6d
nikao8 <nicolasmarquesjf@hotmail.com>
2021-07-28 19:10:54 -0300

    add projeto 3 e 4 csharp

---
+++ CSharp Alura/CSharp parte 1/3-ConversoesEOutrosTiposNumericos.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _3_ConversõesEOutrosTiposNumericos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 3 - Manipulação de variáveis...");
+
+            double salario = 1200.50;
+
+            int salarioInteiro = (int)salario; // Realizando casting de variavel, double -> inteiro
+
+            Console.WriteLine(salarioInteiro);
+
+            long idade = 13000000000; // Diferente do tipo int, que só recebe valores de no maximo 32 bits, o tipo long suporta valores inteiros porém valores muito maiores que o int suporta!.
+            
+            Console.WriteLine(idade);
+            
+            short quantidadeProdutos = 1500; // O tipo short é o oposto do tipo long, ele armazena na memoria apenas 16 bits, usa-se quando a variavel não vai receber valores muito grandes.
+            
+            Console.WriteLine(quantidadeProdutos);
+
+            float altura = 1.80f; // Com o tipo float também é possivel alocar valores decimais, porém ele tem uma precisão de casas depois da virgula menor do que o tipo double.
+            // É necessario colocar o sufico 'f' para variaveis tipo float, assim como no exemplo da variavel altura. 
+
+            Console.WriteLine(altura);
+
+            Console.WriteLine("Pressione enter para finalizar a execução do programa");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CSharp Alura/CSharp parte 1/4-CaracteresETextos.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _4_CaracteresETextos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 4 - Caracteres e textos");
+
+            char primeiraLetra = 'a';// Armazena apenas 1 caractere. aspas simples '' para um caractere & aspas duplas "" para uma string/texto
+            //Armazena apenas 16 bits
+
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char)65; // Dessa forma o numero 65 é representação numérica da letra 'A', de acordo com a ASCII table.
+            
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char)(primeiraLetra + 1);
+
+            Console.WriteLine(primeiraLetra);
+
+            string titulo = "Teste variavel do tipo string..."; // Nota: para texto usa-se aspas duplas "".
+
+            Console.WriteLine(titulo);
+
+            titulo = "Idade = " + 19; // Concatenando ao texto.
+
+            Console.WriteLine(titulo);
+
+            string listinha = "- Item1\n- Item2\n- Item3\n- Item4"; // Utilizando quebra de linha
+
+            Console.WriteLine(listinha);
+            
+            Console.WriteLine("Pressione enter para finalizar a execução do programa...");
+            Console.ReadLine();
+        }
+    }
+}
