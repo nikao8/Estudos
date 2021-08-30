f2679c045cd20e3ca7e102bf76c0808598ed85e4
nikao8 <nicolasmarquesjf@hotmail.com>
2021-08-30 17:55:16 -0300

    add C# pt2

---
+++ CSharp Alura/CSharp parte 2 POO/1-ByteBank/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _1_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+
+            contaDaGabriela.titular = "Gabriela";
+            contaDaGabriela.agencia = 863;
+            contaDaGabriela.numero = 863452;
+            contaDaGabriela.saldo = 100;
+
+            Console.WriteLine("Titular: " + contaDaGabriela.titular);
+            Console.WriteLine("Agência: " + contaDaGabriela.agencia);
+            Console.WriteLine("Número: " + contaDaGabriela.numero);
+            Console.WriteLine("Saldo: " + contaDaGabriela.saldo);
+            Console.ReadLine();
+        }
+    }
+}
