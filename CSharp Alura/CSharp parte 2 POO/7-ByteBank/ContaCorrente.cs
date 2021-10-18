2376d8b16475719975257258fde19c97a196c05e
nikao8 <nicolasmarquesjf@hotmail.com>
2021-10-18 20:25:13 -0300

    add proj 7

---
+++ CSharp Alura/CSharp parte 2 POO/7-ByteBank/ContaCorrente.cs
@@ -0,0 +1,104 @@
+﻿namespace _7_ByteBank
+{
+    public class ContaCorrente
+    {
+
+        private double _saldo = 100; // atribuindo um valor padrão direto na classe!
+
+        /*
+        private Cliente _titular;
+
+        public Cliente Titular
+        {
+            get
+            {
+                return _titular;
+            }
+            set
+            {
+                _titular = value;
+            }
+        }
+        */
+        //simplificação:
+        public Cliente Titular { get; set; }
+
+        private int _agencia;
+        public int Agencia {
+            get
+            {
+                return _agencia;
+            }
+            set
+            {
+                if(value <= 0)
+                {
+                    return;
+                }
+
+                _agencia = value;
+            }
+        }
+        public int Numero { get; set; }
+        // utilizar o 'prop' para agilizar. digitar: prop + tab + tab + tipo + tab + tab + nome propiedade
+        public double Saldo
+        {
+            get
+            {
+                return _saldo;
+            }
+            set // o argumento é dado como value
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+
+                _saldo = value;
+            }
+        }
+
+        public ContaCorrente(int agencia, int numero)
+        {
+            Agencia = agencia;
+            Numero = numero;
+        }
+
+        public bool Sacar(double valor)
+        {
+            if (this._saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                this._saldo -= valor;
+
+                return true;
+            }
+        }
+
+        public void Depositar(double valor)
+        {
+            this._saldo += valor;
+        }
+
+        public bool Tranferir(double valor, ContaCorrente contaDestino)
+        {
+            if (this._saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                this._saldo -= valor;
+                contaDestino.Depositar(valor);
+                return true;
+            }
+        }
+    }
+}
+
+
+
+
---
+++ CSharp Alura/CSharp parte 2 POO/7-ByteBank/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _7_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(867,86712540);
+
+            conta.Agencia = -10;
+
+            Console.WriteLine(conta.Agencia);
+            Console.WriteLine(conta.Numero);
+
+            Console.ReadLine();
+        }
+    }
+}
