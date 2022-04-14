aadb2dd5747d7bec77545d19ce5769a85d918dc0
Nicolas Marques <nicolasmarquesjf@hotmail.com>
2022-04-14 10:09:15 -0300

    Add files via upload
    upload versao teste hotkeys

---
+++ Python/hotkey_setas_v2.py
@@ -0,0 +1,35 @@
+# Versão 2 teste com Listener
+from pynput.keyboard import Key, Controller
+from pynput import keyboard
+
+keyboard1 = Controller()
+
+def on_press(tecla):
+    def on_activate_up():    
+        keyboard1.press(Key.up)
+        keyboard1.release(Key.up)
+
+    def on_activate_down():
+        keyboard1.press(Key.down)
+        keyboard1.release(Key.down)
+
+    def on_activate_left():
+        keyboard1.press(Key.left)
+        keyboard1.release(Key.left)
+
+    def on_activate_right():
+        keyboard1.press(Key.right)
+        keyboard1.release(Key.right)
+
+    with keyboard.GlobalHotKeys({
+            '<alt_gr>+i': on_activate_up,
+            '<alt_gr>+k': on_activate_down,
+            '<alt_gr>+j': on_activate_left,
+            '<alt_gr>+l': on_activate_right
+            }) as h:
+        h.join()
+
+with keyboard.Listener(
+        on_press=on_press,
+        on_release=on_press) as listener:
+    listener.join()
