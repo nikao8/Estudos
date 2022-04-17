488d725360cdd9d39953da261fd9cc1fd4b9ec51
nikao8 <nicolasmarquesjf@hotmail.com>
2022-04-17 16:33:19 -0300

    add map.js

---
+++ JavaScript Alura/1-Fundamentos/map.js
@@ -0,0 +1,6 @@
+// alterando o array com o metodo map
+let nomes = ['Ana JULIA','pedro','CARLA','feLIpe'] 
+
+const nomesAjustados = nomes.map(nome => nome.toUpperCase())
+
+console.log(nomesAjustados)
