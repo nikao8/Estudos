e87a9d6f661820c25c534c30fa55f99112670b05
nicolas.marques <nicolas.marques@CELULA.LOCAL>
2022-08-29 12:35:56 -0300

    Add ex1

---
+++ ADS Vianna/1 SEMESTRE/FP/FP03_100/ex1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ex1
+{
+    class Program
+    {
+
+        static void CalculaTempo(int segundos)
+        {
+            int minutos,horas,segundos1;
+
+            horas = segundos / 3600;
+            minutos = (segundos % 3600) / 60;
+            segundos1 = (segundos % 3600) % 60;
+
+            Console.WriteLine("{0}hr {1}min {2}seg",horas,minutos,segundos1);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("INSIRA O TEMPO EM SEGUNDOS: ");
+            int segundos = Int32.Parse(Console.ReadLine());
+
+            CalculaTempo(segundos);
+        }
+    }
+}
