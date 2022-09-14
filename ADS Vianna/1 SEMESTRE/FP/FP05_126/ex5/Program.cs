f959260c4a689249669aca9e69b4e3db6a12f050
nicolas.marques <nicolas.marques@CELULA.LOCAL>
2022-09-14 17:33:02 -0300

    add ex5

---
+++ ADS Vianna/1 SEMESTRE/FP/FP05_126/ex5/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ex5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.Write("Insira um numero inteiro e positivo: ");
+            num = Int32.Parse(Console.ReadLine());
+            
+            double e = 0;
+            
+            for(int i = 0; i < num; i++)
+            {
+                e = e + (1/CalculaFatorial(i));
+            }
+
+            Console.WriteLine("E = {0}", e);
+        }
+
+        static int CalculaFatorial(int num)
+        {
+            int aux = num;
+            int fatorial = 1;
+
+            while(aux > 0)
+            {
+                fatorial = fatorial * aux;
+                aux--;
+            }
+
+            return fatorial;
+        }
+    }
+}
