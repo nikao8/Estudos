925be5f7616cadd3c1308bbde1d88c785736f1d8
Nicolas Marques <nicolasmarquesjf@hotmail.com>
2022-09-14 21:18:16 -0300

    Add files via upload

---
+++ ADS Vianna/1 SEMESTRE/FP/FP05_126/ex6/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ex6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //int i = 0;
+            
+            int maior; int menor;
+
+            Console.WriteLine("Insira um numero: ");
+            int n = Int32.Parse(Console.ReadLine());
+            
+            menor = n;
+            maior= n;
+
+            while (true)
+            {
+                Console.WriteLine("Insira um numero: ");
+                n = Int32.Parse(Console.ReadLine());
+
+                if(n == 0)
+                {
+                    Console.WriteLine("O maior valor é: {0}\nO menor valor é {1}", maior, menor);
+                    return;
+                }
+
+                if(n < menor)
+                {
+                    menor = n;
+                }
+                else if(n > maior)
+                {
+                    maior = n;
+                }
+            }
+        }
+    }
+}
