c09ee7ec6169b533a1320245707e4ed15c78eb74
Nicolas Marques <nicolasmarquesjf@hotmail.com>
2022-09-19 19:08:25 -0300

    Update Program.cs

--- ADS Vianna/1 SEMESTRE/FP/FP05_126/ex7/Program.cs
+++ ADS Vianna/1 SEMESTRE/FP/FP05_126/ex7/Program.cs
@@ -6,7 +6,7 @@ namespace ex7
     {
         static void Main(string[] args)
         {
-            for (int i = 0; i < 150; i++)
+            for (int i = 50; i <= 150; i++)
             {
                 Console.WriteLine("{0}°F em °C: {1:N2}", i, ((i-32)/(1.8)));
             }
