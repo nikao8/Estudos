b1457d5706bfcbe8990231444f0d7a0f6fdccdd9
Nicolas Marques <nicolasmarquesjf@hotmail.com>
2022-09-20 19:57:34 -0300

    Add files via upload

---
+++ ADS Vianna/1 SEMESTRE/FP/Lista exercicios resolvidos 1/ex1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insira um numero e descubra se ele é par ou não: ");
+            int num = Int32.Parse(Console.ReadLine());
+
+            CalculaPar(num);
+        }
+
+        static void CalculaPar(int n)
+        {
+            if(n % 2 == 0)
+            {
+                Console.WriteLine("{0} é PAR!", n);
+            }
+            else
+            {
+                Console.WriteLine("{0} é IMPAR!", n);
+            }
+        }
+    }
+}
