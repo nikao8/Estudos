f7efddb72c64e67066f4ee3bda7786889c107e36
nicolas.marques <nicolas.marques@CELULA.LOCAL>
2022-09-27 14:25:24 -0300

    exs prova antiga

---
+++ ADS Vianna/1 SEMESTRE/FP/Lista exercicios prova antiga/ex1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insira o valor do produto: ");
+            double valorProd = Double.Parse(Console.ReadLine());
+
+            ImprimeValor(valorProd);
+        }
+
+        static void ImprimeValor(double valor)
+        {
+            if(valor >= 100.0 && valor <= 300.0)
+            {
+                valor = valor - (valor * 0.20);
+            }
+            else if(valor > 300.0)
+            {
+                valor = valor - (valor * 0.30);
+            }
+
+            Console.WriteLine("O valor final do produto: {0}", valor);
+        }
+    }
+}
