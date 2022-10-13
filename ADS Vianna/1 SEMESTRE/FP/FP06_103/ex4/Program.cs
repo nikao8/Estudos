fba5b8a26c79ae79aae8bdd85f754bbd5f96d553
nicolas.marques <nicolas.marques@CELULA.LOCAL>
2022-10-13 11:08:05 -0300

    update ultimos exercicios lista vetores

---
+++ ADS Vianna/1 SEMESTRE/FP/FP06_103/ex4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ex4
+{
+    class Program
+    {
+        const int tamVet = 10;
+
+        static void Main(string[] args)
+        {
+            int[] idades = new int[tamVet];
+            double media = 0;
+            
+            for(int i = 0; i < tamVet; i++)
+            {
+                Console.Write("Insira a idade {0} de {1}: ", i+1, tamVet);
+                idades[i] = Int32.Parse(Console.ReadLine());
+
+                media += idades[i];
+            }
+            
+            Console.WriteLine("{0} idades maiores que a média.", CalculaIdadeMaiorMedia(idades,media));
+        
+        }
+
+        static int CalculaIdadeMaiorMedia(int[] idades, double media)
+        {
+            int contador = 0;
+            media = media / tamVet;
+
+            for(int i = 0; i < tamVet; i++)
+            {
+                if(idades[i] > media)
+                {
+                    contador++; 
+                }
+            }
+
+            return contador;
+        }
+    }
+}
