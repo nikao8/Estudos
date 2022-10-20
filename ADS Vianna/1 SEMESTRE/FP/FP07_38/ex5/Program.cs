4bf8bd2bc16cdc9b71eb5c30752af20a6eb4377b
Nicolas Marques <nicolasmarquesjf@hotmail.com>
2022-10-19 21:49:43 -0300

    Add files via upload

---
+++ ADS Vianna/1 SEMESTRE/FP/FP07_38/ex5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ex5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insira seu texto: ");
+            string txt = Console.ReadLine();
+
+            if(txt.Length <= 1)
+            {
+                Console.Write("Erro! string com um caractere ou menos");
+                return;
+            }
+
+            for(int i = 0; i < txt.Length; i++)
+            {
+                if(i==0 || txt[i-1] == ' ')
+                {
+                    Console.Write(Convert.ToChar(txt[i] - 32));
+                    continue;
+                }
+                Console.Write(txt[i]);
+            }
+        }
+    }
+}
