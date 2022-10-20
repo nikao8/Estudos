e776ee5739c75dca752ee10b17b0bdd8470e6eab
Nicolas Marques <nicolasmarquesjf@hotmail.com>
2022-10-19 21:44:39 -0300

    Add files via upload

---
+++ ADS Vianna/1 SEMESTRE/FP/FP07_38/ex3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insira seu texto: ");
+            string txt = Console.ReadLine();
+            int contadorVogal = 0;
+
+            for(int i = 0;i < txt.Length; i++ )
+            {
+                if(txt[i] == 'a' || txt[i] == 'e' || txt[i] == 'i' || txt[i] == 'o' || txt[i] == 'u')
+                {
+                    contadorVogal++;
+                }
+            }
+
+            Console.WriteLine("{0} vogais.", contadorVogal);
+        }
+    }
+}
