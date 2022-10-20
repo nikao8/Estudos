bb969fc957a871dc04e8b9dde78ab4871beff953
nicolas.marques <nicolas.marques@CELULA.LOCAL>
2022-10-20 11:18:30 -0300

    update FP06_112 add exs

---
+++ ADS Vianna/1 SEMESTRE/FP/FP06_112/ex4/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace ex4
+{
+    class Program
+    {
+        // FORMA SIMPLIFICADA NO FINAL, SEM MOSTRAR TODOS OS INDICES.
+        const int tamVet = 15;
+        static void Main(string[] args)
+        {
+            int[] valores = new int[tamVet];
+            int buscar, contador = 0, j = 0;
+            
+
+            for(int i = 0;i < valores.Length; i++)
+            {   
+                Console.Write("Insira o valor {0} de {1}: ", i+1, tamVet);
+                valores[i] = Int32.Parse(Console.ReadLine());
+            }
+
+            Console.Write("Insira o valor a ser buscado no vetor: ");
+            buscar = Int32.Parse(Console.ReadLine());
+
+            for(int i = 0; i < valores.Length; i++)
+            {
+                if(buscar == valores[i])
+                {
+                    contador++;
+                }
+            }
+
+            int[] indices = new int[contador];
+
+            for(int i = 0; i < valores.Length; i++)
+            {
+                if(buscar == valores[i])
+                {
+                    indices[j] = i+1;
+                    j++;
+                }
+            }
+
+            if(contador != 0)
+            {
+                Console.Write("o valor {0} foi encontrado no vetor {1} vez(s)", buscar, contador);
+                
+                Console.Write(" nos indices: ");
+                for(int i = 0; i< indices.Length; i++)
+                {
+                    if(i == indices.Length-1)
+                    {
+                        Console.Write(indices[i]);
+                    }
+                    else
+                    {
+                        Console.Write(indices[i] + ",");
+                    }              
+                }
+            }
+            else
+            {
+                Console.WriteLine("o valor {0} não foi encontrado no vetor.", buscar);
+            }
+        }
+
+        /* // SEGUNDA FORMA, SEM MOSTRAR TODOS OS INDICES:
+        const int tamVet = 15;
+        static void Main(string[] args)
+        {
+            int[] valores = new int[tamVet];
+            int buscar, contador = 0;
+            for(int i = 0;i < valores.Length; i++)
+            {   
+                Console.Write("Insira o valor {0} de {1}: ", i+1, tamVet);
+                valores[i] = Int32.Parse(Console.ReadLine());
+            }
+
+            Console.Write("Insira o valor a ser buscado no vetor: ");
+            buscar = Int32.Parse(Console.ReadLine());
+
+            for(int i = 0; i < valores.Length; i++)
+            {
+                if(buscar == valores[i])
+                {
+                    contador++;
+                }
+            }
+
+            if(contador != 0)
+            {
+                Console.WriteLine("o valor {0} foi encontrado no vetor {1} vez(s).", buscar, contador);
+            }
+            else
+            {
+                Console.WriteLine("o valor {0} não foi encontrado no vetor.", buscar);
+            }
+        }
+        */
+    }
+}
---
+++ ADS Vianna/1 SEMESTRE/FP/FP06_112/ex5/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ex5
+{
+    class Program
+    {
+        const int tamVet = 15;
+        static void Main(string[] args)
+        {
+            int[] valores = new int[tamVet];
+            int buscar;
+            
+            for(int i = 0;i < valores.Length; i++)
+            {   
+                Console.Write("Insira o valor no indice {0} de {1}: ", i, tamVet-1);
+                valores[i] = Int32.Parse(Console.ReadLine());
+            }
+
+            Console.Write("Insira o valor a ser buscado no vetor: ");
+            buscar = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("O valor buscado foi encontrado no indice {0}.", ProcuraValor(valores, buscar));
+
+        }
+
+        static int ProcuraValor(int[] valores, int buscar)
+        {
+            int indice = -1;
+            for(int i = 0; i < valores.Length; i++)
+            {
+                if(buscar == valores[i])
+                {
+                    indice = i;
+                }
+            }
+
+            return indice;
+        }
+    }
+}
