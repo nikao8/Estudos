543b2703ec9ce2ee049ab60ff19621eeb22b3cdd
nicolas.marques <nicolas.marques@CELULA.LOCAL>
2022-11-04 17:07:31 -0300

    update exercicios

---
+++ ADS Vianna/1 SEMESTRE/FP/FP07_46/ex4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insira o texto: ");
+            string txt = Console.ReadLine();
+
+            string vogais = "", consoantes = "";
+
+            txt = txt.ToLower();
+            for(int i = 0; i < txt.Length; i++)
+            {
+                
+                if(txt[i] == 'a' || txt[i] == 'e' || txt[i] == 'i' || txt[i] == 'o' || txt[i] == 'u')
+                {
+                    vogais += txt[i];
+                }
+                else
+                {
+                    consoantes += txt[i];
+                }
+            }
+
+            Console.WriteLine("String vogais: {0}\nString consoantes: {1}",vogais,consoantes);
+        }
+    }
+}
---
+++ ADS Vianna/1 SEMESTRE/FP/FP07_46/ex5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ex5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insira o texto: ");
+            string txt = Console.ReadLine();
+
+            Console.Write("Insira o caractere para remover do texto: ");
+            char carac = Char.Parse(Console.ReadLine());
+
+            for(int i = 0; i < txt.Length; i++)
+            {
+                if(txt[i] != carac)
+                {
+                    Console.Write(txt[i]);
+                }
+            }
+        }
+    }
+}
