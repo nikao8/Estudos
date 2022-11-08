475d519fed865a8a1f89963ef80aca825edd594a
Nicolas Marques <nicolasmarquesjf@hotmail.com>
2022-11-08 20:16:25 -0300

    Add files via upload

---
+++ ADS Vianna/1 SEMESTRE/FP/Revisao segunda prova/ex2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("insira o texto: ");
+            string txt = Console.ReadLine();
+
+            Console.Write("O texto somente com consoantes é: {0}", Consoantes(txt));
+        }
+
+        static string Consoantes(string txt)
+        {
+            string consoantes = "";
+            for(int i = 0; i < txt.Length; i++)
+            {
+                if(txt[i] != 'a' && txt[i] != 'e' && txt[i] != 'i' && txt[i] != 'o' && txt[i] != 'u')
+                {
+                    consoantes += txt[i];
+                }
+            }
+
+            return consoantes;
+        }
+    }
+}
---
+++ ADS Vianna/1 SEMESTRE/FP/Revisao segunda prova/ex3/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insira a quantidade de linhas da matriz: ");
+            int rows = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Insira a quantidade de colunas da matriz: ");
+            int cols = Convert.ToInt32(Console.ReadLine());
+
+            int[,] mat = new int[rows,cols];
+
+            for(int i = 0; i < mat.GetLength(0); i++)
+            {
+                Console.WriteLine("Linha {0}:\n", i+1);
+                for(int j = 0; j < mat.GetLength(1); j++)
+                {   
+                    Console.Write("Valor {0}: ",j+1);
+                    mat[i,j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            for(int i = 0; i < mat.GetLength(0); i++)
+            {
+                for(int j = 0; j < mat.GetLength(1); j++)
+                {   
+                    Console.Write(mat[i,j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("A soma das diagonais é:");
+            ImprimeDiagSecMatriz(mat);
+        }
+
+        static void ImprimeDiagSecMatriz(int[,] mat)
+        {
+            if(mat.GetLength(0) != mat.GetLength(1))
+            {
+                Console.WriteLine("A matriz precisa ser quadrada!");
+            }
+            else
+            {
+
+                double soma = 0;
+                for(int i = 0; i < mat.GetLength(0); i++)
+                {
+                    for(int j = 0; j < mat.GetLength(1); j++)
+                    {
+                        if(i+j == mat.GetLength(0)-1)
+                        {
+                            soma += mat[i,j];
+                        }
+                    }
+                }
+
+                Console.Write("\n{0}",soma);   
+            }
+        }
+    }
+}
