713ab188b2985d7916587d350121479108aed181
nicolas.marques <nicolas.marques@CELULA.LOCAL>
2022-11-08 15:35:21 -0300

    add exs fp08 matrizes

---
+++ ADS Vianna/1 SEMESTRE/FP/FP08_41/ex4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[,] mat1 = new double[,] {{4,5}, {1,8}, {9,10.5}};
+
+            Console.WriteLine("Soma dos elementos da matriz: {0}", SomaMatriz(mat1));
+        }
+
+        static double SomaMatriz(double[,] mat)
+        {
+            double soma = 0;
+            for(int i = 0; i < mat.GetLength(0); i++)
+            {
+                for(int j = 0; j < mat.GetLength(1); j++)
+                {
+                    soma += mat[i,j];
+                }
+            }
+
+            return soma;
+        }
+    }
+}
---
+++ ADS Vianna/1 SEMESTRE/FP/FP08_41/ex5/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ex5
+{
+    class Program
+    {
+        const int tamanho = 3;
+        static void Main(string[] args)
+        {
+            int[] vet = new int[tamanho];
+            int[,] mat = new int[tamanho,tamanho];
+
+            Console.WriteLine("Preencha o vetor: ");
+            for(int i = 0; i < vet.Length; i++)
+            {
+                Console.Write("Valor {0}:", i);
+                vet[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            
+            for(int i = 0; i < mat.GetLength(0); i++)
+            {
+                Console.WriteLine("Linha {0}: ", i);
+                for(int j = 0; j < mat.GetLength(1); j++)
+                {
+                    Console.Write("Valor {0}: ",j);
+                    mat[i,j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            MultiplicaVetorPelaMatriz(vet,mat);
+        }
+
+        static void MultiplicaVetorPelaMatriz(int[] vet, int[,] mat)
+        {
+            for(int i = 0; i < mat.GetLength(0); i++)
+            {
+                for(int j = 0; j < mat.GetLength(1); j++)
+                {
+                    mat[i,j] = vet[i] * mat[i,j];
+                }
+            }
+
+            Console.WriteLine("Matriz multiplicada:");
+            for(int i = 0; i < mat.GetLength(0); i++)
+            {
+                for(int j = 0; j < mat.GetLength(1); j++)
+                {
+                    Console.Write(mat[i,j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
