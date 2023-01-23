07d6b5bd5088057e9ab72c5cc98ee217fc07a265
Nikao <nikao@pop-os.localdomain>
2023-01-23 15:21:55 -0300

    update vars-lacos-fluxos

---
+++ Java Alura/sintaxe-variaveis-e-fluxos/src/TestaWhile.java
@@ -0,0 +1,16 @@
+
+public class TestaWhile {
+
+	public static void main(String[] args) {
+		
+		int max = 10;
+		int contador = 0;
+		
+		while(contador <= 10)
+		{
+			System.out.println(contador);
+			contador++;
+		}
+	}
+
+}
