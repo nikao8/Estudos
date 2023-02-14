a2fd43da25e3dd9dc8aa62e33a5dc8a881485653
nicolas.marques <nicolas.marques@CELULA.LOCAL>
2023-02-14 09:57:37 -0300

    add algoritimo recursivo semana 1

---
+++ ADS Vianna/2 PERIODO/ED/semana1-algoritimo_recursivo/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace semana1_algoritimo_recursivo
+{
+    /*
+        RESOLVENDO FATORIAL COM RECURSIVIDADE:
+        LÓGICA:
+
+        4! = 4 * 3 * 2 * 1
+        3! = 3 * 2 * 1
+        2! = 2 * 1
+        1! = 1
+        0! = 1
+
+        Logo:
+
+        4! = 4 * 3!
+        3! = 3 * 2!
+        2! = 2 * 1!
+        1! = 1 * 0!
+    */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insira o numero para descobrir seu fatorial: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("O fatorial de {0} é: {1}.", n, Fatorial(n));
+        }
+
+        static int Fatorial(int n)
+        {
+            if(n == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return n * Fatorial(n-1);
+            }
+        }
+    }
+}
