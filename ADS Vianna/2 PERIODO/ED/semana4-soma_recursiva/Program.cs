09cff7e0dc4e578027cfd3c2544e092007d2fdc4
nicolas.marques <nicolas.marques@CELULA.LOCAL>
2023-02-28 11:41:42 -0300

    add desafio função de soma recursiva

---
+++ ADS Vianna/2 PERIODO/ED/semana4-soma_recursiva/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace semana4_soma_recursiva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vetor = {1,2,5};
+            int result = Somar(vetor,0,0);
+
+            Console.WriteLine("Resultado da soma recursiva: " + result);
+        }
+
+        static int Somar(int[] vetor, int indice = 0, int inicio = 0)
+        {
+            if(indice >= vetor.Length)
+            {
+                return inicio;
+            }
+            else
+            {
+                inicio += vetor[indice];
+                return Somar(vetor, indice + 1, inicio);
+            }
+        }
+    }
+}
