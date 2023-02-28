4670cbea02c5d574cad40dac64249279bf296c3e
nicolas.marques <nicolas.marques@CELULA.LOCAL>
2023-02-28 12:32:06 -0300

    add ex topico 02

---
+++ ADS Vianna/2 PERIODO/TWA/exercicio_topico02/exercicio.js
@@ -0,0 +1,39 @@
+// Aluno: Nicolas Marques Pacheco
+
+/*
+- Declare uma variável contendo uma string 
+- Declare uma variável contendo um número dentro de uma string
+- Declare uma variável com a sua idade 
+- Declare outra variável para contar a frase: Meu nome é SEU NOME e minha idade é SUA IDADE
+- Declare  outras duas variáveis, uma  com seu sobrenome e com seu nome exiba as mesmas juntas
+- Coloque a seguinte frase em uma variável: It's study  time
+- Verifique o tipo da variável que contém o seu nome
+*/
+
+
+//- Declare uma variável contendo uma string 
+let texto = "teste";
+
+//- Declare uma variável contendo um número dentro de uma string
+let numString = "8";
+
+//- Declare uma variável com a sua idade
+let idade = 21;
+
+//- Declare outra variável para contar a frase: Meu nome é SEU NOME e minha idade é SUA IDADE
+let nome = "Nicolas";
+let apresentacao = "Meu nome é " + nome + ", e minha idade é " + idade + " anos";
+
+//- Declare  outras duas variáveis, uma  com seu sobrenome e com seu nome exiba as mesmas juntas
+let sobrenome = "Marques Pacheco";
+console.log(nome + " " + sobrenome);
+
+//- Coloque a seguinte frase em uma variável: It's study time
+texto = "It's study time";
+
+//- Verifique o tipo da variável que contém o seu nome
+console.log(typeof(nome));
+
+
+
+
