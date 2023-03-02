2956e3af612798feb2bf276cc8eade4a2accdf0c
nicolas.marques <nicolas.marques@CELULA.LOCAL>
2023-03-02 12:31:46 -0300

    add emp

---
+++ ADS Vianna/2 PERIODO/POO/exercicio_poo_empresa-funcionario/src/Funcionario.java
@@ -0,0 +1,41 @@
+public class Funcionario {
+    double salarioBruto;
+    boolean valeTransporte;
+    boolean trabalhadorNoturno;
+    int qtdFilhos;
+
+    public Funcionario(double salarioBruto, boolean valeTransporte, boolean trabalhadorNoturno, int qtdFilhos)
+    {
+        this.salarioBruto = salarioBruto;
+        this.valeTransporte = valeTransporte;
+        this.trabalhadorNoturno = trabalhadorNoturno;
+        this.qtdFilhos = qtdFilhos;
+    }
+
+    public double CalculaSalarioBruto(double salarioBruto, boolean valeTransporte, boolean trabalhadorNoturno, int qtdFilhos)
+    {
+        double salarioFinal = salarioBruto - (salarioBruto * 0.13);
+
+        if(valeTransporte)
+        {
+            salarioFinal = salarioFinal - (salarioBruto * 0.06);
+        }
+
+        if(trabalhadorNoturno)
+        {
+            salarioFinal = salarioFinal + (salarioBruto * 0.05);
+        }
+
+        if(qtdFilhos > 3)
+        {
+            salarioFinal = salarioFinal + 150.0;
+        }
+        else
+        {
+            salarioFinal = salarioFinal + (qtdFilhos * 50.0);
+        }
+
+        return salarioFinal;
+    }
+
+}
---
+++ ADS Vianna/2 PERIODO/POO/exercicio_poo_empresa-funcionario/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    /*
+    A empresa Gustil possui varios funcionarios
+    A empresa deseja saber o quanto paga para todos funcionarios.
+    Osalario do funcionario e calculado da sequinte forma:
+    Salario Bruto - 13% de inss - 6% se usar vale transporte + 5% se for trabalhador noturno.
+    Para cada filho a empresa fornece um bonus de
+    R$ 50,00 por filho, ate o limite de 3 filhos.
+    */
+    public static void main(String[] args) {
+        Empresa gustil = new Empresa();
+
+        Funcionario func1 = new Funcionario(1500.0,true,false,2);
+        Funcionario func2 = new Funcionario(2500.0,false,true,5);
+        Funcionario func3 = new Funcionario(1800.0,true,true,1);
+    }
+}
