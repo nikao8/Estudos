facb34820f29e5eb39434f983ac98d28ab85d556
Nikao <nikao@pop-os.localdomain>
2023-03-25 16:00:15 -0300

    update

--- ADS Vianna/2 PERIODO/TWA/exercicio_topico05/script.js
+++ ADS Vianna/2 PERIODO/TWA/exercicio_topico05/script.js
@@ -16,23 +16,3 @@ for(let i = 0; i < frutas.length; i++)
     console.log(frutas[i]);
 }
 
-/*
- Exercicios de perguntas e respostas:
-
- ex1:
-
- O codigo de ex1 retorna com erro pois não foi declarado uma função corretamente com a palavra reservada function e a sintaxe correta, exemplo: function teste(){}
- Além disso no console.log não é possivel printar a palavra reservada var, que é usada para declaração de variaveis(não houve declaração), seria possivel printar var
- se estivesse entre aspas simples ou duplas, indicando uma string.
-
- ex2:
-
- Na função dividirDois a variavel "dois não existe", só existe no escopo da função somarDois, fora dela a variável não existe.
- 
- ex3:
-
- Trocar o nome da variavel numero declarada na primeira linha, pois ela esta sendo subescrita dentro do for, pois ela já existe fora do laço, quando é chamada no loop
- o javascript utiliza a variável já criada.
-
-*/ 
-
