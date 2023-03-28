ee65c93352af1e88c44dab6349b13949906af03c
Nicolas Marques <nicolasmarquesjf@hotmail.com>
2023-03-27 21:53:20 -0300

    Add files via upload
    add lista 1

---
+++ ADS Vianna/2 PERIODO/ED/lista_exercicios_1/ex6/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Insira o numero para descobrir seu fatorial: ");
+        int numero = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Soma dos numeros anteriores a {0} é: {1}", numero, Soma(numero));
+    }
+
+    static int Soma(int num)
+    {
+        if(num > 0)
+        {
+            return num + Soma(num - 1);
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}
