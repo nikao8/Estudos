e39833ea9580f6b5a08beaac0919a87f72714b2f
nicolas.marques <nicolas.marques@CELULA.LOCAL>
2023-03-28 16:10:27 -0300

    add ex8

---
+++ ADS Vianna/2 PERIODO/ED/lista_exercicios_1/ex8/Program.cs
@@ -0,0 +1,26 @@
+﻿internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] vet = new int[] {5,15,89,3,74,9};
+
+        Console.WriteLine("O menor valor do vetor é: {0}", Minimo(vet, vet.Length - 1, vet[vet.Length - 1]));
+    }
+
+    static int Minimo(int[] vet, int i, int menor)
+    {
+        if(i < 0)
+        {
+            return menor;
+        }
+
+        if(vet[i] < menor)
+        {
+            menor = vet[i];
+        }
+
+        return Minimo(vet, i-1, menor);
+    }
+
+
+}
