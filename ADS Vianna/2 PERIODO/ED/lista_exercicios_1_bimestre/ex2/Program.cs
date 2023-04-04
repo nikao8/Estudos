d04fb0fce0c6b4d6ebf7faa94f1b1ab169b5e1f3
nikao8 <nicolasmarquesjf@hotmail.com>
2023-04-03 21:45:19 -0300

    add ex2

---
+++ ADS Vianna/2 PERIODO/ED/lista_exercicios_1_bimestre/ex2/Program.cs
@@ -0,0 +1,38 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int cont = 0;
+        int[] vet = new int[]{8,-3,-2,5,3,-5,3};
+        Console.WriteLine("A quantidade de numeros negativos encontrado é: {0}", PercorreVetor(vet));
+
+        Console.WriteLine("O vetor é: ");
+        for(int i = 0; i < vet.Length; i++)
+        {
+            Console.Write(vet[i] + " ");
+        }
+    }
+    /* // utilizando a função contadora
+    static int PercorreVetor(int[] vet)
+    {
+        return PercorreVetor(vet,0,0);
+    }
+    */
+    static int PercorreVetor(int[] vet, int i=0, int contador=0)
+    {
+        if(i >= vet.Length)
+        {
+            return contador;
+        }
+
+        if(vet[i] < 0)
+        {
+            vet[i] = vet[i] * -1;
+            contador++;
+        }
+
+        return PercorreVetor(vet, i+1, contador);
+    }
+
+ 
+}
