a9927df306d0364f8aa74bd3e198a1d5acf6ec97
nikao8 <nicolasmarquesjf@hotmail.com>
2023-04-04 14:58:32 -0300

    add ex4

---
+++ ADS Vianna/2 PERIODO/ED/lista_exercicios_1_bimestre/ex4/Program.cs
@@ -0,0 +1,44 @@
+﻿internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] vetor = new int[] {9,6,32,1,7,5,8};
+
+        Console.Write("Vetor antes de ordenar: ");
+        for(int i = 0; i < vetor.Length; i++)
+        {
+            Console.Write(vetor[i] + " ");
+        }
+
+        MetodoBubbleSort(vetor);
+
+        Console.Write("\nVetor depois de ordenar: ");
+        for(int i = 0; i < vetor.Length; i++)
+        {
+            Console.Write(vetor[i] + " ");
+        }
+    }
+
+
+    static void MetodoBubbleSort(int[] vet)
+    {
+        for(int i = 1; i < vet.Length; i++)
+        {
+            for(int j = 0; j < vet.Length - 1; j++)
+            {
+                if(vet[j] > vet[j + 1])
+                {
+                    Troca(vet, j);
+                }
+            }
+        }
+    }
+
+    static void Troca(int[] vet, int j)
+    {
+        int aux;
+        aux = vet[j];
+        vet[j] = vet[j + 1];
+        vet[j + 1] = aux;
+    }
+}
