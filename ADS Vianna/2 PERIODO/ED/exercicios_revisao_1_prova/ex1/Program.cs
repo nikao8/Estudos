7677f07702a76208edc1c3e2d7218fa993085f4a
nikao8 <nicolasmarquesjf@hotmail.com>
2023-04-17 17:43:50 -0300

    updt

---
+++ ADS Vianna/2 PERIODO/ED/exercicios_revisao_1_prova/ex1/Program.cs
@@ -0,0 +1,31 @@
+﻿internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] vet = new int[]{35,22,68,15,78,7,74};
+
+        Bubblesort(vet);
+
+        for(int i = 0; i < vet.Length; i++){
+            Console.Write(vet[i] + " ");
+        }
+    }
+
+    static void Bubblesort(int[] vet)
+    {
+        int j = 0, aux;
+        
+        while(vet.Length > j++){
+
+            for(int i = 0; i < vet.Length - 1; i++){
+               if(vet[i] > vet[i+1])
+               {
+                    aux = vet[i];
+                    vet[i] = vet[i+1];
+                    vet[i+1] = aux;
+               } 
+            }
+            //j++;
+        }
+    }
+}
---
+++ ADS Vianna/2 PERIODO/ED/exercicios_revisao_1_prova/ex3/Program.cs
@@ -0,0 +1,30 @@
+﻿internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] vet = new int[]{35,22,68,15,78,7,74};
+
+        Insercao(vet);
+
+        for(int i = 0; i < vet.Length; i++){
+            Console.Write(vet[i] + " ");
+        }    
+    }
+
+    static void Insercao(int [] vet){
+        int aux, j;
+
+        for(int i = 1; i < vet.Length; i++){
+
+            aux = vet[i];
+            j = i;
+
+            while(j > 0 && aux < vet[j - 1]){
+                vet[j] = vet[j - 1];
+                j--;
+            }
+
+            vet[j] = aux;
+        }
+    }
+}
