56cdc0748915870a1de5c3b6ece138fa20e24b5d
nikao8 <nicolasmarquesjf@hotmail.com>
2023-07-25 12:15:52 -0300

    add projeto inicial do desafio

---
+++ Java Alura/2-AplicandoOO/desafio_cartao/src/br/com/alura/desafio/model/Cartao.java
@@ -0,0 +1,55 @@
+package br.com.alura.desafio.model;
+
+import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Cartao {
+
+    private String numero;
+    private double limite;
+    private ArrayList<Compra> compras;
+
+
+    public Cartao(double limite) {
+        this.numero = generateRandomCardNumber();
+        this.limite = limite;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public double getLimite() {
+        return limite;
+    }
+
+    public ArrayList<Compra> getCompras() {
+        return compras;
+    }
+
+    private boolean contabilizaCompra(Compra c){
+        if(c.getPreco() > this.limite){
+            return false;
+        }
+
+        this.limite -= c.getPreco();
+        this.compras.add(c);
+        return true;
+    }
+
+    private String generateRandomCardNumber(){
+        String numero = "";
+        int maxDigits = 16;
+
+        for(int i = 0; i < maxDigits; i++){
+            if(i % 4 == 0 && i != maxDigits - 1){
+                numero += " ";
+            }
+
+            numero += String.valueOf(ThreadLocalRandom.current().nextInt(0, 9));
+        }
+        ThreadLocalRandom.current().nextInt();
+
+        return numero;
+    }
+}
---
+++ Java Alura/2-AplicandoOO/desafio_cartao/src/br/com/alura/desafio/model/Compra.java
@@ -0,0 +1,22 @@
+package br.com.alura.desafio.model;
+
+public class Compra {
+    private String nome;
+    private double preco;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+}
