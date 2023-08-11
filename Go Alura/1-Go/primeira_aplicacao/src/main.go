8bc0e6b3f7bd807b75b94bbb8260f0bedc403d7b
nikao8 <nicolasmarquesjf@hotmail.com>
2023-08-11 12:37:07 -0300

    add progresso primeira-aplicacao

--- Go Alura/1-Go/primeira_aplicacao/src/main.go
+++ Go Alura/1-Go/primeira_aplicacao/src/main.go
@@ -4,62 +4,97 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"time"
 )
+
+const ondas = 5
+const delay = 5
 
 func main() {
 
-	exibeMenu()
+	for {
+		exibeMenu()
 
-	op := solicitaLeitura()
+		op := solicitaLeitura()
+		fmt.Println("")
 
-	switch op {
-	case 1:
-		iniciarMonitoramento(retornaListaSites())
+		switch op {
+		case 1:
+			iniciarMonitoramento(retornaListaSites())
 
-	case 2:
-		fmt.Println("Exibindo logs...")
+		case 2:
+			fmt.Println("Exibindo logs...")
 
-	case 0:
-		fmt.Print("Finalizando programa...")
-		os.Exit(0)
+		case 0:
+			fmt.Print("Finalizando programa...")
+			os.Exit(0)
 
-	default:
-		fmt.Println("Comando", op, " não reconhecido...")
-		os.Exit(-1)
+		default:
+			fmt.Println("Comando", op, " não reconhecido...")
+			os.Exit(-1)
+		}
 	}
 
 }
 
 func retornaListaSites() []string {
-	const tam = 3
-	var list [tam]string
 
-	for i := 0; i < len(list); i++ {
-		fmt.Println("Insira um site [", i+1, "de", len(list), "] para adicionar a lista de monitoramento:")
+	list := []string{}
+
+	for {
+		fmt.Println("Insira um site para adicionar a lista de monitoramento:")
 
 		var site string
 		fmt.Scan(&site)
-		list[i] = site
+		list = append(list, site)
+
+		fmt.Println("Deseja adicionar mais sites? 1-Sim | 2-Não")
+		var esc int
+		fmt.Scan(&esc)
+
+		if esc != 1 {
+			break
+		}
 
 	}
 
-	return list[:]
+	return list
 }
 
 func iniciarMonitoramento(sites []string) {
 	fmt.Println("Monitorando...")
+	fmt.Println("")
 
-	for i := 0; i < len(sites); i++ {
-		resp, err := http.Get(sites[i])
-
-		if err != nil {
-			fmt.Println("Houve um erro ao requisitar o site", sites[i], ":")
-			fmt.Println(err.Error())
-		} else {
-			fmt.Println("Resposta de ", sites[i], ":", resp)
+	/*
+		for i := 0; i < len(sites); i++ {
+			testaSite(site[i])
 		}
+	*/
+
+	//usando for com range (poderia colocar o contador i ao inves de _ caso fosse necessário trabalhar com o contador)
+
+	for i := 0; i < ondas; i++ {
+
+		fmt.Println("Onda de monitoramento: [", i+1, "/", ondas, "]")
+		for _, site := range sites {
+			testaSite(site)
+		}
+
+		fmt.Println("")
+		time.Sleep(delay * time.Second)
 	}
 
+}
+
+func testaSite(site string) {
+	resp, err := http.Get(site)
+
+	if err != nil {
+		fmt.Println("Houve um erro ao requisitar o site", site, ":")
+		fmt.Println(err.Error())
+	} else {
+		fmt.Println("StatusCode de ", site, ":", resp.Status)
+	}
 }
 
 func exibeMenu() {
