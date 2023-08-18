211ef69514f57fc8dc0e12fed53de4578323b729
nikao8 <nicolasmarquesjf@hotmail.com>
2023-08-18 17:12:49 -0300

    add progresso curso 4 api com golang

--- Go Alura/4-Go API Rest/api-rest/controllers/controllers.go
+++ Go Alura/4-Go API Rest/api-rest/controllers/controllers.go
@@ -1,10 +1,16 @@
 package controllers
 
 import (
+	"api-rest/models"
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Home Page")
+}
+
+func TodasPersonalidades(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(models.Personalidades)
 }
--- Go Alura/4-Go API Rest/api-rest/main.go
+++ Go Alura/4-Go API Rest/api-rest/main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"api-rest/models"
 	"api-rest/routes"
 	"fmt"
 )
 
 func main() {
-	fmt.Println("Hello World")
 
+	models.Personalidades = []models.Personalidade{
+		{Nome: "Nome 1", Historia: "Historia 1"},
+		{Nome: "Nome 2", Historia: "Historia 2"},
+	}
+
+	fmt.Println("Iniciando o servidor...")
 	routes.HandleRequest()
 }
