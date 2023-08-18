9f85694c696c0402ed460d98ae6b0f17f92df441
nikao8 <nicolasmarquesjf@hotmail.com>
2023-08-18 16:17:07 -0300

    add progresso curso 4 golang api rest

---
+++ Go Alura/4-Go API Rest/api-rest/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"api-rest/routes"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello World")
+
+	routes.HandleRequest()
+}
---
+++ Go Alura/4-Go API Rest/api-rest/routes/routes.go
@@ -0,0 +1,12 @@
+package routes
+
+import (
+	"api-rest/controllers"
+	"log"
+	"net/http"
+)
+
+func HandleRequest() {
+	http.HandleFunc("/", controllers.Home)
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
