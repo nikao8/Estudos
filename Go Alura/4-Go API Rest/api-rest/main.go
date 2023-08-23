39c9594efae8beda3f106f2e562a3dae2fafc156
Nicolas Marques <nicolasmarquesjf@hotmail.com>
2023-08-23 17:35:48 -0300

    Update main.go

--- Go Alura/4-Go API Rest/api-rest/main.go
+++ Go Alura/4-Go API Rest/api-rest/main.go
@@ -8,11 +8,6 @@ import (
 
 func main() {
 
-	models.Personalidades = []models.Personalidade{
-		{Id: 1, Nome: "Nome 1", Historia: "Historia 1"},
-		{Id: 2, Nome: "Nome 2", Historia: "Historia 2"},
-	}
-
 	fmt.Println("Iniciando o servidor...")
 	routes.HandleRequest()
 
